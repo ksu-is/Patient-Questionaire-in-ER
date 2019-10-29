72d0fb4111494603d095a2be256864732b37e0c8
Alexis Nixon <54323389+anixon13@users.noreply.github.com>
2019-10-29 18:07:46 -0400

    Update and rename Starting code(first third) to Starting code(first third).py

---
+++ Starting code(first third).py
@@ -0,0 +1,24 @@
+import time    
+import pickle
+import os
+class Hospital:
+    def _init_(self):
+        self.sno=0
+        self.name=' '
+        self.age=0
+        self.sex=""
+        self.email=" "
+        self.fname=" "
+        self.address=''
+        self.city=''
+        self.state=''
+        self.height=0
+        self.weight=0
+        self.doctor=''
+        self.med=''
+        self.bill=0
+        self.paymethod=''
+        self.pno=0
+        self.bgroup=''
+        self.dname=''
+
