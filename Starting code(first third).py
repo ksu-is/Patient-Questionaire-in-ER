5c34426a7e580a8385713c38dded6d9243703a70
Alexis Nixon <54323389+anixon13@users.noreply.github.com>
2019-10-29 18:09:01 -0400

    Update Starting code(first third).py

--- Starting code(first third).py
+++ Starting code(first third).py
@@ -22,3 +22,42 @@ class Hospital:
         self.bgroup=''
         self.dname=''
 
+ def Input(self):
+        self.sno=input("Enter Serial number:")
+        self.name=raw_input("Enter Patinet's Name:")
+        self.age=input("Enter Patinet's Age:")
+        self.sex=raw_input("Enter Patinet's Sex (Male/Female):")
+        self.height=input("Enter Patinet's Height:")
+        self.weight=input("Enter Patinet's Weight(In Kgs):")
+        self.bgroup=raw_input("Enter Patient's Blood Group:")
+        self.fname=raw_input("Enter Fathers Name:")
+        self.address=raw_input("Enter Address:")
+        self.city=raw_input("Enter City:")
+        self.state=raw_input("Enter State:")
+        self.pno=input("Enter Phone number:")
+        self.email=raw_input("Enter E-Mail:")
+        self.doctor=raw_input("Enter Doctor's Name:")
+        self.dname=raw_input("Enter Disease Name:")
+        self.med=raw_input("Enter Prescribed Medicine:")
+        self.bill=input("Enter Bill Amount: Rs.")
+        self.paymethod=raw_input("Enter Payment Method(Cash/Cheque/Card):")
+        def Output(self):
+        print ("SERIAL NUMBER:-"),self.sno
+        print ("PATIENT'S NAME:-"),self.name
+        print ("PATIENT'S AGE:-"),self.age
+        print ("PATIENT'S SEX:-"),self.sex
+        print ("PATIENT'S HEIGHT:-"),self.height
+        print ("PATIENT'S WEIGHT:-"),self.weight
+        print ("PATIENT'S BLOOD GROUP:-"),self.bgroup
+        print ("FATHERS NAME:-"),self.fname
+        print ("ADDRESS:-"),self.address
+        print ("CITY:-"),self.city
+        print ("STATE:-"),self.state
+        print ("CONTACT NUMBER:-"),self.pno
+        print ("E-MAIL ADDRESS:-"),self.email
+        print ("DISEASE NAME:-"),self.dname
+        print ("DOCTOR'S NAME:-"),self.doctor
+        print ("PRESCRIBED MEDICINES:-"),self.med
+        print ("BILL AMOUNT:-"),self.bill
+        print ("PAYMENT METHOD:-"),self.paymethod
+        
