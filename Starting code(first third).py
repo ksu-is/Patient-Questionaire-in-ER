5480026bc12c12fa876f0548d23901da18351a18
Alexis Nixon <54323389+anixon13@users.noreply.github.com>
2019-10-30 10:51:10 -0400

    Update Starting code(first third).py

--- Starting code(first third).py
+++ Starting code(first third).py
@@ -6,26 +6,28 @@ class Hospital:
         self.name=" "
         self.age=0
         self.sex=" "
-        self.fdrink=" "
         self.doctor=" "
         self.med=" "
         self.allergies=" "
+        self.fdrink=" "
+        self.temp=" "
 
     def Input(self):
         self.name=raw_input("Enter Patinet's Name:")
         self.age=input("Enter Patinet's Age:")
         self.sex=raw_input("Enter Patinet's Sex (Male/Female):")
-        self.fname=raw_input("Enter Favorite Drink:")
         self.doctor=raw_input("Enter Doctor's Name:")
         self.med=raw_input("Enter Prescribed Medicine:")
-        self.allergies=raw_input("Enter Known ALlergies:")
+        self.allergies=raw_input("Enter Known Allergies:")
+        self.fname=raw_input("Enter Favorite Drink:")
+        self.temp=raw_input("Enter last known Temperature:")
  def Output(self):
         print ("PATIENT'S NAME:-"),self.name
         print ("PATIENT'S AGE:-"),self.age
         print ("PATIENT'S SEX:-"),self.sex
-        print ("FAVORITE DRINK:-"),self.fdrink
         print ("DOCTOR'S NAME:-"),self.doctor
         print ("PRESCRIBED MEDICINES:-"),self.med
         print ("Known Allergies:-"),self.allergies
-        
+        print ("FAVORITE DRINK:-"),self.fdrink
+        print ("Last Known Temp:-"),self.temp
         
